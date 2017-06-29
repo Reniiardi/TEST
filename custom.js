50dec60ff0ac19475966495449b074b72ef4fc42
Reniiardi <reniiardi@gmail.com>
2017-06-29 14:02:35 -0500

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,77 @@
+
+<!-------- Custom javascript code, used in the side bar ---->
+<script>
+
+// FLIP ICON ON CLICK 
+$(document).on( 'click', '#subParent', function (){
+    $('#FuncPageIcon').toggleClass('fa-chevron-down fa-chevron-right');
+});
+
+// Do not close Drop down section from child 
+$(document).on( 'click', '.sub-menu', function (e){
+    e.stopPropagation();
+});
+
+// Do not close Drop down section from child 
+
+
+// CheckBox input control, Max 3
+function checkBox(T){
+	if(T.is(':checked')){
+		checkedcount +=1 ;
+		T.attr('SEL',"1");
+		T.parent().attr('SEL',"1");
+		T.parent().next().attr('SEL',"1");
+	//	console.log(checkedcount);
+	} else {
+		checkedcount-=1;
+		
+		T.attr('SEL',"0");
+		T.parent().attr('SEL',"0");
+		T.parent().next().attr('SEL',"0");
+	//	console.log(checkedcount);
+	}
+	
+	if (checkedcount >= 3){
+		document.getElementById('reachedMax').setAttribute('style', "color:red;display:table;");
+			$('#allChecks').find('.form-control[SEL=0]').css("background","GRAY");
+			$('#allChecks').find('.input-group-addon[SEL=0]').css("background","GRAY");
+			$('#allChecks').find('.single-checkbox[SEL=0]').prop('disabled',true);
+			$('#allChecks').find('.form-control[SEL=1]').css("background","#80ff00");
+			$('#allChecks').find('.input-group-addon[SEL=1]').css("background","#80ff00");
+	
+	} else {
+		document.getElementById('reachedMax').setAttribute('style', "color:red;display:none;");
+			$('#allChecks').find('.form-control[SEL=0]').css("background","WHITE");
+			$('#allChecks').find('.input-group-addon[SEL=0]').css("background","WHITE");
+			$('#allChecks').find('.single-checkbox[SEL=0]').removeAttr('disabled');
+			$('#allChecks').find('.form-control[SEL=1]').css("background","#80ff00");
+			$('#allChecks').find('.input-group-addon[SEL=1]').css("background","#80ff00");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
---
+++ expanded.js
@@ -0,0 +1,48 @@
+<script>
+// DISABLE FIELDS NOT REQUESTED ;
+
+var MAJCOM = "&MAJCOM";
+var TOS = "&TOS";
+var TIS = "&TIS";
+var GRADE = "&GRADE";
+var AFSC = "&AFSC";
+var DS = "&DS";
+var FC = "&FC";
+var RS = "&RS";
+var RDTM = "&RS";
+var LOC = "&LOC";
+
+$(document).ready(function (){
+	if (MAJCOM != "0"){
+		$('#dc-MAJ-chart').prop('style',"display:block;");
+	}
+	if (TOS !="0"){
+		$('#dc-TOS-chart').prop('style',"display:block;");
+	}
+	if (TIS != '0'){
+		$('#dc-TIS-chart').prop('style',"display:block;");
+	}
+	if (GRADE != '0'){
+		$('#dc-grade-chart').prop('style',"display:block;");
+	}
+	if (AFSC != '0'){
+		$('#dc-AFSC-chart').prop('style',"display:block;");
+	}
+	if (DS !='0'){
+		$('#dc-DS-chart').prop('style',"display:block;");
+	}
+	if (FC != '0'){
+		$('#dc-FC-chart').prop('style',"display:block;");
+	}
+	if (RS != '0'){
+		$('#dc-RS-chart').prop('style',"display:block;");
+	}
+	if (RDTM != '0'){
+		$('dc-RDTM-chart').prop('style',"display:block;");
+	}
+	if (LOC != '0'){
+		$('dc-location-chart').prop('style',"display:block;");
+	}
+});
+
+</script>
