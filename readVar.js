fc397d7ac5defa42a1b3efb5e2bb438215f2d1f8
Reniiardi <reniiardi@gmail.com>
2017-07-03 10:33:44 -0500

    Update readVar.js

--- readVar.js
+++ readVar.js
@@ -13,33 +13,33 @@ $(document).ready(function (){
 	console.log(LOC);
 	
 	if (MAJCOM != "0"){
-		$('#dc-MAJ-chart').prop('style',"display:block;");
+		$('#dc-MAJ-chart').css("display","block");
 	}
 	if (TOS !="0"){
-		$('#dc-TOS-chart').prop('style',"display:block;");
+		$('#dc-TOS-chart').css("display","block");
 	}
 	if (TIS != '0'){
-		$('#dc-TIS-chart').prop('style',"display:block;");
+		$('#dc-TIS-chart').css("display","block");
 	}
 	if (GRADE != '0'){
-		$('#dc-grade-chart').prop('style',"display:block;");
+		$('#dc-grade-chart').css("display","block");
 	}
 	if (AFSC != '0'){
-		$('#dc-AFSC-chart').prop('style',"display:block;");
+		$('#dc-AFSC-chart').css("display","block");
 	}
 	if (DS !='0'){
-		$('#dc-DS-chart').prop('style',"display:block;");
+		$('#dc-DS-chart').css("display","block");
 	}
 	if (FC != '0'){
-		$('#dc-FC-chart').prop('style',"display:block;");
+		$('#dc-FC-chart').css("display","block");
 	}
 	if (RS != '0'){
-		$('#dc-RS-chart').prop('style',"display:block;");
+		$('#dc-RS-chart').css("display","block");
 	}
 	if (RDTM != '0'){
-		$('dc-RDTM-chart').prop('style',"display:block;");
+		$('dc-RDTM-chart').css("display","block");
 	}
 	if (LOC != '0'){
-		$('dc-location-chart').prop('style',"display:block;");
+		$('dc-location-chart').css("display","block");
 	}
 });
